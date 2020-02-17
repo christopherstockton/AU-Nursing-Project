fc0c2d4c9dd48e96bf8437d70f705ee78a1afa25
christopherstockton <cstockton01@aurora.edu>
2020-02-17 10:08:40 -0600

    git git

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -160,7 +160,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top mb-5">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">AU Nursing</a>
+        <a class="navbar-brand" href="{{ url('/welcome') }}">AU Nursing</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
