85e967ca26be4b4bbc0e722bb305b62e20f15e44
Dave Carpenter <davvcarpenter@gmail.com>
2020-04-08 10:02:35 -0500

    partial bulk upload changes

--- resources/views/people/bulk.blade.php
+++ resources/views/people/bulk.blade.php
@@ -1,28 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <h3>Confirm Course Information</h3>
-        <form>
-            <table class="table table-bordered">
 
-                <tr>
-                    <th>First</th>
-                    <th>Last</th>
-
-                </tr>
-
-                @foreach($rows as $row)
-                    <tr>
-                        @foreach($row as $col)
-                            <td><textarea>{{ $col }}</textarea></td>
+<div class="container">
+  <div class=row>
+    <div class="col-lg-6">
+      <h2 class="mb-3">Confirm Student List</h2>
+        <div>
+            <form>
+                <p>
+                    Add To Class:
+                    <select id="course" name="course" class="form-control">
+                        @foreach ($courses as $course)
+                        <option value="{{$course->id}}">{{$course->CourseSection}}</option>
                         @endforeach
+                    </select>
+                </p>
+                <table class="table table-bordered">
+
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+
                     </tr>
 
-                @endforeach
+                    @foreach($rows as $row)
+                        <tr>
+                            @foreach($row as $col)
+                                <td><input value={{$col}} type="text" class="form-control"></td>
+                            @endforeach
+                        </tr>
 
-            </table>
-            <button type="submit" class="btn btn-primary mb-2">Upload</button>
-        </form>
+                    @endforeach
+
+                </table>
+                <button type="submit" class="btn btn-primary mb-2">Upload</button>
+            </form>
+          </div>
+        @endsection
+      </div>
     </div>
-@endsection
+  </div>
+</div>
